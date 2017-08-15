dd657f40451b6ec5d30a0b1c3b8a4f384f2fa53a
Joshua Wu <joshua.wu.0910@gmail.com>
2017-08-15 01:23:56 -0700

    udpated install script. install jdk8 and 7, create sym link. update alternatives

--- java-setup.sh
+++ java-setup.sh
@@ -2,12 +2,37 @@
 # This will install java versions 1.7 and 1.8
 
 # Directory for Java install
-JAVA_DIR=/opt/java
+# JAVA_DIR=/opt/java
+JAVA_DIR=/home/joshua/programming/liferay/misc/scripts/java-setup/opt/java
+OPT_JAVA_DIR=current
 
-echo "Copying jdk8 to ${JAVA_DIR}"
-sudo cp ./jdk8.tar.gz $JAVA_DIR/jdk8.tar.gz
-sudo tar -zxvf $JAVA_DIR/jdk8.tar.gz
 
-echo "Copying jdk7 to ${JAVA_DIR}"
-sudo cp ./jdk7.tar.gz $JAVA_DIR/jdk7.tar.gz
-sudo tar -zxvf $JAVA_DIR/jdk7.tar.gz
+echo "Copying files to ${JAVA_DIR}"
+cp ./jdk8.tar.gz $JAVA_DIR
+cp ./jdk7.tar.gz $JAVA_DIR
+cd $JAVA_DIR
+
+echo "Unpacking files..."
+tar -zxvf $JAVA_DIR/jdk8.tar.gz
+mv $JAVA_DIR/jdk1.8* $JAVA_DIR/jdk8
+tar -zxvf $JAVA_DIR/jdk7.tar.gz
+mv $JAVA_DIR/jdk1.7* $JAVA_DIR/jdk7
+
+echo "Folders jdk7 and jdk8 created"
+
+# Create sym link pointing to default jdk8
+ln -s $JAVA_DIR/jdk8 current
+echo "Created current symbolic link for default jdk8"
+
+# Update alternatives
+exec<alt-list
+
+while read line
+do
+  update-alternatives --install /usr/bin/$line $line /opt/java/$OPT_JAVA_DIR/bin/$line 2000
+done
+
+
+
+# Cleanup
+rm $JAVA_DIR/*.tar.gz
